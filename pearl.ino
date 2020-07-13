6014b9af511caba05396c08dda8837f2ec0952d5
Calvin <splicc@gmail.com>
2020-07-12 23:03:23 -0700

    new chassis
    postdated 2015-08-04

--- pearl.ino
+++ pearl.ino
@@ -7,84 +7,93 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
 // Create the motor objects
-Adafruit_DCMotor *DCRed = AFMS.getMotor(1); // Red
-Adafruit_DCMotor *DCGreen = AFMS.getMotor(2); // Green
-Adafruit_DCMotor *DCBlue = AFMS.getMotor(3); // Blue
+Adafruit_DCMotor *DCRed = AFMS.getMotor(3); // Red
+Adafruit_DCMotor *DCYellow = AFMS.getMotor(2); // Yellow
+Adafruit_DCMotor *DCBlue = AFMS.getMotor(1); // Blue
+  
+// Declare variables
+uint8_t RED    = 3;
+uint8_t BLUE   = 1;
+uint8_t YELLOW = 2;
 
 void setup() {
-  Serial.begin(9600);
   AFMS.begin();
-  
   delay(2500);
   
-  // Drive forward then backward at speed 50 for 1.5 seconds
-  drive(FORWARD, 150, 750);
-  delay(1500);
-  drive(BACKWARD, 150, 750);
-  delay(1500);  
+
 }
 
 void loop() {
-  spin(FORWARD);
-  delay(500);
-  spin(BACKWARD);
-  delay(500);
-}
-
-void kill() {
-  DCRed->run(RELEASE);
-  DCGreen->run(RELEASE);
-  DCBlue->run(RELEASE);
+  // Drive forward then backward at speed 50 for 1.5 seconds
+  drive(RED, 75, 750);
+  delay(1500);
+  drive(BLUE, 75, 750);
+  delay(1500);
+  drive(YELLOW, 75, 750);
+  delay(1500);
 }
 
 void drive(uint8_t DIRECTION, int SPEED, int DELAY) {
-  //
-  // I need to figure out how to do this section properly
-  //
-  
-  if (DIRECTION == FORWARD) {
-   // Set direction
-   DCGreen->run(FORWARD);
+  if (DIRECTION == RED) {
+   // Set direction and speed
+   DCYellow->run(FORWARD);
    DCBlue->run(BACKWARD);
-  
-   // Drive in Red direction
-   DCGreen->setSpeed(SPEED);
+   DCYellow->setSpeed(SPEED);
    DCBlue->setSpeed(SPEED);
   }
   
-  if (DIRECTION == BACKWARD) {
-   // Set direction
-   DCGreen->run(BACKWARD);
+  if (DIRECTION == BLUE) {
+   // Set direction and speed
+   DCYellow->run(BACKWARD);
+   DCRed->run(FORWARD);
+   DCYellow->setSpeed(SPEED);
+   DCRed->setSpeed(SPEED);
+  }
+  
+  if (DIRECTION == YELLOW) {
+   // Set direction and speed
+   DCRed->run(BACKWARD);
    DCBlue->run(FORWARD);
-  
-   // Drive in Red direction
-   DCGreen->setSpeed(SPEED);
+   DCRed->setSpeed(SPEED);
    DCBlue->setSpeed(SPEED);
   }
   
-  // Pause then kill motors
+  // Allow to drive for DELAY miliseconds then kill motors
   delay(DELAY);
-  kill();
+  halt();
 }
 
 void spin(uint8_t DIRECTION) {
   // Set direction
   DCRed->run(DIRECTION);
-  DCGreen->run(DIRECTION);
+  DCYellow->run(DIRECTION);
   DCBlue->run(DIRECTION);
   
   // Ramp up
-  for (int i=25; i<150; i++) {
+  for (int i=25; i<100; i++) {
     DCRed->setSpeed(i);
     DCBlue->setSpeed(i);
-    DCGreen->setSpeed(i);
-    delay(10);
+    DCYellow->setSpeed(i);
+    delay(25);
   }  
   
   // Ramp down
-  for (int i=150; i>25; i--) {
+  for (int i=100; i>25; i--) {
   DCRed->setSpeed(i);
   DCBlue->setSpeed(i);
-  DCGreen->setSpeed(i);
+  DCYellow->setSpeed(i);
+  delay(25);
   }
+}
+
+void kill() {
+  DCRed->run(RELEASE);
+  DCYellow->run(RELEASE);
+  DCBlue->run(RELEASE);
+}
+
+void halt() {
+  DCRed->setSpeed(0);
+  DCBlue->setSpeed(0);
+  DCYellow->setSpeed(0);
 }
